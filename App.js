97858d804531b3e1156d7557d28f1e1dab2a43f4
Jules <dali1999@naver.com>
2023-09-07 03:54:52 +0900

    프로젝트 생성

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar />
+      <View style={styles.city}>
+        <Text>Seoul</Text>
+      </View>
+      <View style={styles.weather}></View>
+      <View></View>
     </View>
   );
 }
@@ -13,8 +18,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1,
+    backgroundColor: "teal",
   },
 });
