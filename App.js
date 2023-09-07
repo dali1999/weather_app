4fbef475e6c3b0fe5a853700a9c44284efa03b44
Jules <dali1999@naver.com>
2023-09-07 17:31:28 +0900

    build

--- App.js
+++ App.js
@@ -106,7 +106,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#85A1B1",
+    backgroundColor: "red",
   },
   city: {
     flex: 1.2,
